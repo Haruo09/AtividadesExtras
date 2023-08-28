f2402edca087c95eb21f708645161f5787b226c2
Fábio Haruo de Freitas Nishioka <fabio.hn01@gmail.com>
2023-08-27 21:11:54 -0300

    ex1

---
+++ TrabExtra3/src/exercicios/ex1/classes/Pessoa.java
@@ -0,0 +1,41 @@
+package exercicios.ex1.classes;
+
+public class Pessoa {
+    /*
+    * Como o exercício só pediu pra criar os getters
+    * and setters, nem criei a classe main.
+    * */
+    private String nome;
+    private int idade;
+    private String email;
+
+    public Pessoa(String nome, int idade, String email) {
+        this.nome = nome;
+        this.idade = idade;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
